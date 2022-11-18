dfb99f438bf24f0a7ea4aac912cabade8eb8241f
Vinicius Costa <viniciuscostadealmeida1998@gmail.com>
2022-11-18 14:33:19 -0300

    Redimensionando o Header

--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -9,7 +9,7 @@ export const HeaderContainer = styled.div`
     a{
         color: #fff;
         font-family: Poppins;
-        font-size: 2rem;
+        font-size: 1.5rem;
         margin: 1rem 1rem;
 
         &:hover{
