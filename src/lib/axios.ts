52ec383c175a551de9db665a9c9e0d846b1b4d0c
Vinicius Costa <viniciuscosta9898@icloud.com>
2024-04-08 16:14:13 -0300

    Update url on email service

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+  baseURL: 'https://viniciuscosta1998.vercel.app/api',
   headers: {
     'Content-Type': 'application/json',
   },
